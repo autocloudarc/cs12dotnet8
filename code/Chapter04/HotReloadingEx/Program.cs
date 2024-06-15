2f74045df5c416eaef9c25f86e271de0b5242d80
autocloudarc <autocloudarc@outlook.com>
2024-06-15 17:53:49 -0400

    page(222): hot reload

---
+++ code/Chapter04/HotReloadingEx/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Visual Studio 2022: run the app, change the message, click Hot Reload.
+ * Visual Studio Code: run the app using dotnet watch, changt he message, save the file. */
+
+while (true)
+{
+    WriteLine("Goodbye, Hot Reload!");
+    await Task.Delay(2000);
+}
