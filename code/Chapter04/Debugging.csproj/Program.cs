5c8b826a8611d0c4c3fbc2b8bf58a3e315df6d57
autocloudarc <autocloudarc@outlook.com>
2024-06-15 15:39:49 -0400

    page(212): add error to debug

---
+++ code/Chapter04/Debugging.csproj/Program.cs
@@ -0,0 +1,10 @@
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+double Add(double a, double b)
+{
+    return a * b;
+}
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine(); // Wait for user to press Enter.
