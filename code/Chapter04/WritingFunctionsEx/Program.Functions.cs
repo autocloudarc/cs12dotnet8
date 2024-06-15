fae6f713d379fb207ea9252234263058b08da633
autocloudarc <autocloudarc@outlook.com>
2024-06-14 22:49:23 -0400

    page(203): complete ordinal numbers exercise

--- code/Chapter04/WritingFunctionsEx/Program.Functions.cs
+++ code/Chapter04/WritingFunctionsEx/Program.Functions.cs
@@ -1,4 +1,5 @@
-﻿partial class Program
+﻿using System.Globalization; // To use CultureInfo
+partial class Program
 {
     static void TimesTable(byte number, byte size = 12)
     {
@@ -11,4 +12,64 @@
         }
         WriteLine();
     }
+    static decimal CalculateTax(
+        decimal amount, string twoLetterRegionCode)
+    {
+        decimal rate = twoLetterRegionCode switch
+        {
+            "CH" => 0.08M, // Switzerland
+            "DK" or "NO" => 0.25M, // Denmark, Norway
+            "GB" or "FR" => 0.2M, // UK, France
+            "HU" => 0.27M, // Hungary
+            "OR" or "AK" or "MT" => 0.0M, // Oregon, Alaska, Montana
+            "ND" or "WI" or "ME" or "VA" => 0.05M, // North Dakota, Wisconsin, Maine, Virginia
+            "CA" => 0.0825M, // California
+            _ => 0.06M // Most other states.
+        };
+
+        return amount * rate / 100;
+    }
+    static void ConfigureConsole(string culture = "en-US", 
+        bool useComputerCulture = false)
+    {
+        // To enable Unicode characters like Euro symbol in the console.
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useComputerCulture)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        WriteLine($"Current culture is {CultureInfo.CurrentCulture.DisplayName}");
+    }
+    
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+        switch (lastTwoDigits)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return $"{number}th";
+            default:
+                uint lastDigit = number % 10;
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number}{suffix}";
+        }
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (uint number = 1; number <= 1500; number++)
+        {
+            WriteLine($"{CardinalToOrdinal(number)}");
+        }
+        WriteLine();
+    }
+
 }
--- code/Chapter04/WritingFunctionsEx/Program.cs
+++ code/Chapter04/WritingFunctionsEx/Program.cs
@@ -1,1 +1,8 @@
-﻿TimesTable(number: 10, size: 20);
+﻿// TimesTable(number: 10, size: 20);
+// ConfigureConsole(culture: "fr-FR");
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
+// Alternatively, call the function in the interpolated string.
+// WriteLine($"you must pay {CalculateTax(amount: 149,
+// twoLetterRegionCode: "FR"):C} in tax.");
+RunCardinalToOrdinal();
